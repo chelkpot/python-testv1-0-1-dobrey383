783ad1f9255af5c02103809586a76e887973bc07
dobrey383 <nikitakarimov7@gmail.com>
2025-10-01 11:52:20 +0500

    dodelal2

--- tasks/task3.py
+++ tasks/task3.py
@@ -6,7 +6,8 @@ def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
     weight = int(input("Введите первое число:"))
     height = int(input("Введите второе число:"))
-    print(weight/height**2)
+    height2 = height**2
+    print(weight/height2)
    
 
 # Код ниже не трогать! он нужен для тестов
